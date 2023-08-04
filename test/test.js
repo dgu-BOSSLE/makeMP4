399cc7fc1e1b55b8b1599870fabb09a81899840c
bunju20 <ghkd2271@naver.com>
2023-08-04 11:21:29 +0900

    feat : 화면 녹화 및 mp4 url출력 확인
    화면 공유 허락받는 부분수정해야함
    위의 부분 해결 후 원래 웹사이트에 해당 test코드 적용해야함.

---
+++ test/test.js
@@ -0,0 +1,45 @@
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const output = document.getElementById("output");
+
+let mediaRecorder;
+let chunks = [];
+
+startButton.onclick = async function () {
+  const displayMediaOptions = {
+    video: {
+      cursor: "always",
+    },
+    audio: false,
+  };
+
+  try {
+    const stream = await navigator.mediaDevices.getDisplayMedia(
+      displayMediaOptions
+    );
+
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.ondataavailable = (e) => chunks.push(e.data);
+    mediaRecorder.start();
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  } catch (err) {
+    console.error("Error: " + err);
+  }
+};
+
+stopButton.onclick = function () {
+  mediaRecorder.stop();
+
+  mediaRecorder.onstop = function () {
+    const blob = new Blob(chunks, { type: "video/webm" });
+    chunks = [];
+
+    const videoURL = URL.createObjectURL(blob);
+    output.textContent = "Video URL: " + videoURL;
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  };
+};
